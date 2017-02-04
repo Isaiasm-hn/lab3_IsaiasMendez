8a8e9bdf7d9dec56a2d43c46ce11c7ae79d35a90
Isaias Mendez <isaiasm_hn@unitec.edu>
2017-02-03 18:18:36 -0600

    abc

--- lab3.cpp
+++ lab3.cpp
@@ -4,9 +4,15 @@ using namespace std;
 
 void Ejercicio1();
 
+void Ejercicio2();
+
 int** InitMat(int);
 
 int** llenarMat(int,int);
+
+int siglo(int);
+
+int mes(int);
 
 int main(){
 	int ejer;
@@ -24,6 +30,7 @@ int main(){
 			break;
 		case 2:
 			cout<<"------EJERCICIO 2------";
+			Ejercicio2();
 			break;
 	}
 
@@ -35,6 +42,7 @@ void Ejercicio1(){
 	int** tablero=NULL;
 	int** tablero2=NULL;
 	int size;
+	cout<<endl;
 	do{
 		cout<<"Ingrese dimension de la matriz >= 9: ";
 		cin>>size;
@@ -118,4 +126,115 @@ int** llenarMat(int size,int mine){
 		}
 	}*/
 	return mat;
+}
+
+void Ejercicio2(){
+	int a, b, c, dia;
+	int cont=0;
+	int bisiesto;
+	cout<<endl;
+	cout<<"Ingrese dia: ";
+	cin>>a;
+	cout<<"Ingrese mes: ";
+	cin>>b;
+	cout<<"Ingrese year: ";
+	cin>>c;
+	cout<<endl<<endl;
+	switch (b){	
+		case 2:
+				if(c%400==0 || c%4==0 && c%100!=0){
+						bisiesto=1;
+						dia=29;
+					}else{
+						dia=28;
+						bisiesto=0;	
+					}
+					break;
+			case 4:
+					dia=30;
+					break;
+			case 6:
+					dia=30;
+					break;
+			case 9:
+					dia=30;
+					break;
+			case 11:
+					dia=30;
+					break;
+			default:
+					dia=31;
+					break;	
+	}
+	
+		int sum;
+		switch (b){
+			case 1:
+					sum=0;
+					break;
+			case 2:
+					sum=31;
+					break;
+			case 3: 
+					sum=59+bisiesto;
+					break;
+			case 4:
+					sum=90+bisiesto;
+					break;
+			case 5:
+					sum=120+bisiesto;
+					break;
+			case 6:
+					sum=151+bisiesto;
+					break;
+			case 7:
+					sum=181+bisiesto;
+					break;
+		
+			case 8: 
+					sum=212+bisiesto;
+					break;
+			case 9:
+					sum=243+bisiesto;
+					break;
+			case 10:
+					sum=273+bisiesto;
+					break;
+			case 11:
+					sum=304+bisiesto;
+					break;
+			case 12:
+					sum=334+bisiesto;
+					break;
+
+		}
+		int d,e,y,r;
+		d=sum+a;
+		e=(c-1)/4;
+		r=y%7;
+		switch (r){
+			case 0:
+				    cout<<a<<"/"<<b<<"/"<<c<<" Es Viernes";
+					break;
+			case 1:
+					cout<<a<<"/"<<b<<"/"<<c<<" Es sabado";
+					break;
+			case 2:
+					cout<<a<<"/"<<b<<"/"<<c<<" Es domingo";
+					break;
+			case 3:
+					cout<<a<<"/"<<b<<"/"<<c<<" Es lunes";
+					break;
+			case 4:
+					cout<<a<<"/"<<b<<"/"<<c<<" es martes";
+					break;
+			case 5:		
+					cout<<a<<"/"<<b<<"/"<<c<<" es miercoles";
+					break;
+			case 6:
+					cout<<a<<"/"<<b<<"/"<<c<<" Es jueves";
+					break;
+			
+		}
+		cout<<endl;
 }
